619af51517c56708179d025c97726913cb9dad11
y-singh09 <153745633+y-singh09@users.noreply.github.com>
2024-02-25 00:41:34 +0530

    Merge Intervals Problem(Leetcode problem number 56)
    Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
    Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
    Output: [[1,6],[8,10],[15,18]]
    Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].

---
+++ java/merge_interval.java
@@ -0,0 +1,44 @@
+
+import java.util.Arrays;
+import java.util.ArrayList;
+public class merge_interval {
+
+	public static void main(String[] args) {
+		int [][] a={{1,3},{2,6},{8,10},{15,18}};
+		int[][] ans = merged_array(a);
+		for(int[] i : ans)
+		{
+			System.out.print(Arrays.toString(i));  // It is used to print the merged array
+		}
+		
+	}
+	static int[][] merged_array(int[][] a)
+	{
+		if(a.length==0||a==null)
+		{
+			return new int[0][0];
+		}
+		Arrays.sort(a,(m,n)->m[0]-n[0]);  // It is used to sort on the basis of m and n jaha m bara hai aur n chota
+		ArrayList<int[]> list=new ArrayList<int[]>();
+		list.add(a[0]);
+		for(int i=1;i<a.length;i++)
+		{
+			int[] current=a[i];
+			int[] last=list.get(list.size()-1);
+			if(current[0]<=last[1])
+			{
+				last[1]=Math.max(current[1], last[1]);
+			}
+			else {
+				list.add(current);
+			}
+		}
+		int[][] res=new int[list.size()][2];
+		for(int i=0;i<list.size();i++)
+		{
+			res[i]=list.get(i);
+		}
+		return res;
+	}
+
+}
