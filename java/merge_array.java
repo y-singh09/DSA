16cc87390d1f52b582c242b9eb290b6e98e5eaab
YUVRAAJ SINGH <153745633+y-singh09@users.noreply.github.com>
2024-05-26 20:14:18 +0530

    Merge Sorted Array(Leetcode Problem no:-88)
    Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
    Output: [1,2,2,3,5,6]
    Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
    The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.

---
+++ java/merge_array.java
@@ -0,0 +1,62 @@
+package DSA;
+
+public class merge_array {
+
+    public static void main(String[] args) {
+        int a[] = {-1,0,0,3,3,3,0,0,0}; 
+        int b[]= {2,4,6};
+        int m = a.length;  
+        int n = b.length; 
+
+        
+        merge(a, m, b, n);
+
+       
+        }
+   
+
+   //Zeros from the array se hatane ke liye ek function:
+    
+    
+    public static int countMeaningfulElements(int[] a) {
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != 0) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+
+//Merge sort
+    public static void merge(int[] a, int m, int[] b, int n) {
+        int i = 0, j = 0, k = 0;
+        int p=countMeaningfulElements(a);
+       int q=n;
+       int c[] = new int[p+q];  // New array to hold the merged result
+        // Merge the arrays
+        while (i < p && j < q) {
+            if (a[i] < b[j]) {
+                c[k++] = a[i++];
+            } else {
+                c[k++] = b[j++];
+            }
+        }
+
+        // Bacha hua elements
+        while (i < p) {
+            c[k++] = a[i++];
+        }
+
+       
+        while (j < q) {
+            c[k++] = b[j++];
+        }
+        // Print the merged array
+        for (int num : c) {
+            System.out.print(num + " ");
+    }
+}
+}
